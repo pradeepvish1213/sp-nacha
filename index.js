8962e1124b8894a42ccd8dc478bcc086c77203f0
PradeepVishwakarma <pradeepvish1213@gmail.com>
2023-10-06 12:10:20 +0530

    Npm Package Update - fix Added IAT Record Entry and Batch entry

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports.File = require('./lib/file');
 module.exports.Entry = require('./lib/entry');
 module.exports.Batch = require('./lib/batch');
+module.exports.BatchIAT = require('./lib/batch-iat');
+module.exports.EntryIAT = require('./lib/entry-iat');
 module.exports.Utils = require('./lib/utils');
 module.exports.Validate = require('./lib/validate');
 module.exports.EntryAddenda = require('./lib/entry-addenda');
--- test/generateIatRecord.js
+++ test/generateIatRecord.js
@@ -1,167 +1,212 @@
-const Entry = require("../lib/entry");
-let data = [
-    {
-        "id": 67,
-        "immediateDestination": "031300012",
-        "immediateOrigin": "121042882",
-        "immediateDestinationName": "PNCBANK, N.A.",
-        "immediateOriginName": "WELLS FARGO BANK NA",
-        "referenceCode": " ",
-        "batchChildren": [
-            {
-                "id": 67,
-                "companyName": "Compan Name",
-                "companyIdentification": null,
-                "serviceClassCode": "200",
-                "standardEntryClassCode": "IAT",
-                "companyDiscretionaryData": null,
-                "companyEntryDescription": "Accounting",
-                "companyDescriptiveDate": "",
-                "effectiveEntryDate": "2023-10-04T00:00:00.000Z",
-                "settlementDate": null,
-                "originatingDFI": "121042882",
-                "entryChildren": [
-                    {
-                        "id": 67,
-                        "receivingDFI": "031300012",
-                        "DFIAccount": "12345643212354211",
-                        "amount": "1000",
-                        "idNumber": null,
-                        "individualName": "",
-                        "discretionaryData": "",
-                        "transactionCode": "27",
-                        "transactionType": "Debit",
-                        "addendaRecords": [
-                            {
-                                "id": 69,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "10",
-                                "payment_related_information": "UAS123454323454320.00werw                  ~~~                                ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 70,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "11",
-                                "payment_related_information": "Pradeep                            Tahne                              ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 71,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "12",
-                                "payment_related_information": "Tahen                              INDIA                      ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 72,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "13",
-                                "payment_related_information": "demo                               12q23453                            USA",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 73,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "14",
-                                "payment_related_information": "sdfsfsdf                           12wesfdsfsdf                        USA",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 74,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "15",
-                                "payment_related_information": "2345trgfvdswegfThane                              ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 75,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "16",
-                                "payment_related_information": "Thane Pradeep Vishwakarma          4000400000                         ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 76,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "17",
-                                "payment_related_information": "payment_related_information   ",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            },
-                            {
-                                "id": 77,
-                                "queued_transaction_id": 67,
-                                "addenda_type_code": "18",
-                                "payment_related_information": "IDBI BANK                          ISTHANE                             USA",
-                                "created_at": "2023-10-03 17:37:10",
-                                "modified_at": "2023-10-03 17:37:46"
-                            }
-                        ],
-                        "k": {
-                            "id": 67,
-                            "client_id": 0,
-                            "matrix_id": 6294,
-                            "unique_id": "cf3c0f74-1e8a-4e35-9bb6-28e423a2ecff",
-                            "api_mode": "A",
-                            "rule_id": "",
-                            "status": 0,
-                            "review": null,
-                            "service_class_code": "200",
-                            "routing_number": "121042882",
-                            "company_name": "Compan Name",
-                            "company_identification": null,
-                            "company_discretionary": null,
-                            "company_entry_description": "Accounting",
-                            "foreign_exchange_indicator": "FF",
-                            "foreign_exchange_reference_indicator": "5",
-                            "foreign_exchange_reference": "USA",
-                            "iso_destination_country_code": "CA",
-                            "iso_originating_currency_code": "IND",
-                            "iso_destination_currency_code": "CAD",
-                            "effective_entry_date": "2023-10-04 05:30:00",
-                            "settlement_date": null,
-                            "transaction_code": "27",
-                            "receiving_dfi": "031300012",
-                            "dfi_account_number": "12345643212354211",
-                            "amount": "1000",
-                            "individual_id_number": null,
-                            "individual_name": null,
-                            "check_serial_number": null,
-                            "process_control_field": null,
-                            "item_research_number": null,
-                            "card_expiration_date": null,
-                            "document_reference_number": null,
-                            "individual_card_account_number": null,
-                            "terminal_city": null,
-                            "terminal_state": null,
-                            "ach_return_code": null,
-                            "ach_file_name": null,
-                            "entry_detail_type": "IAT",
-                            "discretionary_data": null,
-                            "created_at": "2023-10-03 17:37:10",
-                            "modified_at": "2023-10-03 17:37:46",
-                            "processed_at": null
-                        }
-                    }
-                ]
-            }
-        ],
-        "recordCount": 1
-    }
-]
+let chai = require('chai')
+    , expect = chai.expect
+    , FS = require("fs")
+    , NachaAimPoint = require("../index")
+    , path = require("path")
+    , Moment = require("moment")
+    , EntryAddenda = require("../lib/entry-addenda")
+
+let queuedTransaction = [{
+    "id": 70,
+    "immediateDestination": "231380104",
+    "immediateOrigin": "121042882",
+    "immediateDestinationName": "CITADEL FED C U",
+    "immediateOriginName": "WELLS FARGO BANK NA",
+    "referenceCode": " ",
+    "batchChildren": [{
+        "id": 70,
+        "serviceClassCode": "200",
+        "iatIndicator": "Iat Indiacator p",
+        "foreignExchangeIndicator": "IN",
+        "foreignExchangeReferenceIndicator": "3",
+        "foreignExchangeReference": "pradeepvish1213",
+        "isoDestinationCountryCode": "US",
+        "originatorIdentification": "2345432443",
+        "standardEntryClassCode": "IAT",
+        "companyEntryDescription": "SANdeepviS",
+        "isoOriginatingCurrencyCode": "IND",
+        "isoDestinationCurrencyCode": "CAD",
+        "effectiveEntryDate": Moment().toDate(),
+        "originatorStatusCode": "",
+        "originatingDFI": "121042882",
+        "entryChildren": [{
+            "id": 70,
+            "receivingDFI": "231380104",
+            "DFIAccount": "23456787654323",
+            "amount": "92345678",
+            "idNumber": null,
+            "individualName": "",
+            "discretionaryData": "",
+            "transactionCode": "21",
+            "transactionType": "Credit",
+            "traceNumber": '000000001234567',
+            "addendaRecords": [{
+                "id": 78,
+                "queued_transaction_id": 70,
+                "addenda_type_code": "10",
+                "payment_related_information": "UAS123454323454323.008765432345670000000001The abbreviation INDN on a bank sta",
+                "created_at": "2023-10-05 20:35:59",
+                "modified_at": "2023-10-05 20:35:59"
+            }]
+        }, {
+            "id": 70,
+            "receivingDFI": "231380104",
+            "DFIAccount": "23456787654323",
+            "amount": "92345678",
+            "idNumber": null,
+            "individualName": "",
+            "discretionaryData": "",
+            "transactionCode": "21",
+            "transactionType": "Credit",
+            "traceNumber": '000000001234567',
+            "addendaRecords": [{
+                "id": 78,
+                "queued_transaction_id": 70,
+                "addenda_type_code": "10",
+                "payment_related_information": "UAS123454323454323.008765432345670000000001The abbreviation INDN on a bank sta",
+                "created_at": "2023-10-05 20:35:59",
+                "modified_at": "2023-10-05 20:35:59"
+            }, {
+                "id": 78,
+                "queued_transaction_id": 70,
+                "addenda_type_code": "11",
+                "payment_related_information": "UAS123454323454323.008765432345670000000001The abbreviation INDN on a bank sta",
+                "created_at": "2023-10-05 20:35:59",
+                "modified_at": "2023-10-05 20:35:59"
+            }, {
+                "id": 78,
+                "queued_transaction_id": 70,
+                "addenda_type_code": "12",
+                "payment_related_information": "UAS123454323454323.008765432345670000000001The abbreviation INDN on a bank sta",
+                "created_at": "2023-10-05 20:35:59",
+                "modified_at": "2023-10-05 20:35:59"
+            }]
+        }]
+    }],
+    "recordCount": 1
+}]
 
 describe('IAT Records', function () {
-    describe('Create Entry', function () {
-        it('should create an entry successfully', function () {
-             console.log('===============Started')
+    describe('Create IAT Entry', function () {
+        it('should create an IAT entry successfully', function () {
+            let entry = new NachaAimPoint.EntryIAT(queuedTransaction[0].batchChildren[0].entryChildren[0]);
+            entry.generateString(function (string) {
+                console.log(string);
+            });
+        });
+    });
+    describe('Create IAT Entry with addenda', function () {
+        it('should create an entry with an addenda successfully', function () {
+            let entry = new NachaAimPoint.EntryIAT(queuedTransaction[0].batchChildren[0].entryChildren[0]);
+            let addenda = new EntryAddenda(queuedTransaction[0].batchChildren[0].entryChildren[0].addendaRecords);
+            expect(entry.getRecordCount()).to.equal(1);
+            entry.addAddenda(addenda);
+            expect(entry.get('addendaId')).to.equal('1');
+            expect(entry.getRecordCount()).to.equal(2);
+            expect(addenda.get('addendaSequenceNumber')).to.equal(1);
+            expect(addenda.get('entryDetailSequenceNumber')).to.equal('1234567');
+            let addenda2 = new EntryAddenda(queuedTransaction[0].batchChildren[0].entryChildren[1].addendaRecords);
+            entry.addAddenda(addenda2);
+            expect(entry.get('addendaId')).to.equal('1');
+            expect(entry.getRecordCount()).to.equal(3);
+            expect(addenda.get('addendaSequenceNumber')).to.equal(1);
+            expect(addenda.get('entryDetailSequenceNumber')).to.equal('1234567');
+            entry.generateString(function (string) {
+                console.log(string);
+            });
+        });
+    });
+
+    describe('Create IAT Entry', function () {
+        it('should create an IAT entry successfully', function () {
+            queuedTransaction.forEach(({batchChildren, id, ...restField}) => {
+                try {
+                    let Nacha2AimPointFile = new NachaAimPoint.File(restField);
+                    let totalBatchNumber = batchChildren.length;
+                    let totalCreditAmount = 0
+                    let totalDebitAmount = 0;
+                    let successRecords = []
+                    let errorBatchEntryRecords = []
+                    batchChildren.forEach(({entryChildren, id, ...restField}) => {
+                        let batch = new NachaAimPoint.BatchIAT(restField);
+                        entryChildren.forEach(({transactionType, addendaRecords, id, ...entry}) => {
+                            if (transactionType === 'Credit') {
+                                totalCreditAmount = totalCreditAmount + parseInt(entry.amount)
+                            } else {
+                                totalDebitAmount = totalDebitAmount + parseInt(entry.amount)
+                            }
+
+                            try {
+                                let entryRecord = new NachaAimPoint.EntryIAT(entry);
+                                if (addendaRecords && addendaRecords.length > 0) {
+                                    addendaRecords.forEach(addenda => {
+                                        let addendaEntry = new NachaAimPoint.EntryAddenda({
+                                            paymentRelatedInformation: addenda.payment_related_information
+                                        });
+                                        entryRecord.addAddenda(addendaEntry);
+                                    })
+                                }
+                                batch.addEntry(entryRecord);
+                                successRecords.push({row_id: id, message: 'Success', error: false})
+                            } catch (e) {
+                                console.log({row_id: id, message: e.message, error: true, errorStack: e})
+                            }
+                        })
+                        try {
+                            Nacha2AimPointFile.addBatch(batch);
+                        } catch (e) {
+                            console.log({row_id: id, message: e.message, error: true, errorStack: e})
+                        }
+                    })
+                    let fileName = `ACH${restField.immediateOrigin}PEIN${Moment().format('YYYYMMDDHHmmssSS')}.ach`;
+                    if (successRecords.length > 0) {
+                        Nacha2AimPointFile.generateFile(function (result) {
+                            FS.writeFile(path.join('./test/ach_file', fileName), result, function (error) {
+                                if (error) {
+                                    console.log({
+                                        message: error.message ? error.message : error.stack,
+                                        error: true,
+                                        errorStack: error
+                                    })
+                                }
+                                if (errorBatchEntryRecords.length === 0) {
+                                    console.log({
+                                        error: false,
+                                        message: 'IAT Record Successfully writing file.',
+                                        fileName,
+                                        totalBatchNumber,
+                                        totalDebitAmount,
+                                        totalCreditAmount,
+                                        endTime: Moment().format('YYYY-MM-DD HH:mm:ss'),
+                                        successRecords,
+                                        errorBatchEntryRecords
+                                    })
+                                }
+                            });
+                        });
+                    }
+                    if (errorBatchEntryRecords.length > 0) {
+                        console.log({
+                            error: true,
+                            fileName,
+                            message: 'IAT Record Queued transaction has some error',
+                            totalBatchNumber,
+                            totalDebitAmount,
+                            totalCreditAmount,
+                            endTime: Moment().format('YYYY-MM-DD HH:mm:ss'),
+                            successRecords,
+                            errorBatchEntryRecords
+                        })
+                    }
+                } catch (error) {
+                    console.log({
+                        row_id: id,
+                        message: error.message ? error.message : error.stack,
+                        error: true,
+                        errorStack: error
+                    })
+                }
+            })
         });
     });
 })
